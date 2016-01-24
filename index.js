a0c8f137dca18cf0dbfa0a1ed97264985b869e02
mgenkin <genkin.matt@gmail.com>
2016-01-24 00:19:48 -0500

    added randomized background

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
-	$("p").click(function(){
-		alert( "Handler for .click() called." );
-	});
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	$("#bgcanvas").attr("width", window.innerWidth);
+	$("#bgcanvas").attr("height", window.innerHeight);
+	var c = document.getElementById("bgcanvas");
+	console.log(window.innerWidth)
+	var ctx = c.getContext("2d");
+	var img_array = [];
+	for(var i = 0; i < 9; i++){
+		for(var j = 0; j < 9; j++){
+			var img = new Image();
+			img.src = "blocks/block"+i+"_"+j+".jpg";
+			img_array.push(img);
+		};
+	};
+	var w_im = img_array[0].width;
+	var h_im = img_array[0].height;
+	var i = 0;
+	var j = 0;
+	var r =  Math.floor((Math.random() * 90)); 
+	while(j<=w){
+		while(i<=h){
+			r =  Math.floor((Math.random() * 30));
+			ctx.drawImage(img_array[r], j, i);
+			i += h_im;
+		};
+		j += w_im;
+		i = 0;
+	};
 });
